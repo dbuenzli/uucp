dbuenzli/uucp

commit 6d54a937df103abdd67519868b2792edcc933468

Track cmdliner.

B0.ml
@@ -68,7 +68,7 @@ let default =
     |> tag B0_opam.tag
     |> add B0_opam.Meta.depopts [ "uutf", ""; "uunf", ""; "cmdliner", ""]
     |> add B0_opam.Meta.conflicts [ "uutf", {|< "1.0.1"|};
-                                    "cmdliner", {|< "1.0.0"|}]
+                                    "cmdliner", {|< "1.1.0"|}]
     |> add B0_opam.Meta.depends
       [ "ocaml", {|>= "4.03.0"|};
         "ocamlfind", {|build|};

test/ucharinfo.ml
@@ -513,9 +513,9 @@ let uspec =
 
 let doc = "Query Unicode character information"
 let exits =
-  Term.exit_info 1 ~doc:"if the character specification is invalid." ::
-  Term.exit_info 2 ~doc:"if a key doesn't exist." ::
-  Term.default_exits
+  Cmd.Exit.info 1 ~doc:"if the character specification is invalid." ::
+  Cmd.Exit.info 2 ~doc:"if a key doesn't exist." ::
+  Cmd.Exit.defaults
 
 let man = [
   `S Manpage.s_description;
@@ -583,10 +583,12 @@ let man = [
      $(i,http://www.unicode.org/reports/tr44/)"; ]
 
 let ucharinfo =
-  Term.(const ucharinfo $ cmd $ keys $ spec_fmt $ out_fmt $ uspec),
-  Term.info "ucharinfo" ~version:"%%VERSION%%" ~doc ~exits ~man
+  Cmd.v (Cmd.info "ucharinfo" ~version:"%%VERSION%%" ~doc ~exits ~man)
+    Term.(const ucharinfo $ cmd $ keys $ spec_fmt $ out_fmt $ uspec)
 
-let () = Term.(exit_status @@ eval ucharinfo)
+
+let main () = exit (Cmd.eval' ucharinfo)
+let main () = if !Sys.interactive then () else main ()
 
 (*---------------------------------------------------------------------------
    Copyright (c) 2017 The uucp programmers

opam
@@ -1,31 +1,7 @@
 opam-version: "2.0"
 name: "uucp"
-synopsis: """Unicode character properties for OCaml"""
-maintainer: ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
-authors: ["The uucp programmers"]
-homepage: "https://erratique.ch/software/uucp"
-doc: "https://erratique.ch/software/uucp/doc/"
-dev-repo: "git+https://erratique.ch/repos/uucp.git"
-bug-reports: "https://github.com/dbuenzli/uucp/issues"
-license: ["ISC"]
-tags: ["unicode" "text" "character" "org:erratique"]
-depends: ["ocaml" {>= "4.03.0"}
-          "ocamlfind" {build}
-          "ocamlbuild" {build}
-          "topkg" {build & >= "1.0.3"}
-          "uucd" {with-test}
-          "uunf" {with-test}
-          "uutf" {with-test}]
-depopts: ["uutf"
-          "uunf"
-          "cmdliner"]
-conflicts: ["uutf" {< "1.0.1"}
-            "cmdliner" {< "1.0.0"}]
-build: [["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"
-         "--with-uutf" "%{uutf:installed}%"
-         "--with-uunf" "%{uunf:installed}%"
-         "--with-cmdliner" "%{cmdliner:installed}%" ]]
-description: """
+synopsis: "Unicode character properties for OCaml"
+description: """\
 Uucp is an OCaml library providing efficient access to a selection of
 character properties of the [Unicode character database][1].
 
@@ -35,5 +11,41 @@ dependencies. It is distributed under the ISC license.
 [1]: http://www.unicode.org/reports/tr44/
 
 Home page: http://erratique.ch/software/uucp"""
-post-messages: ["If the build fails with \"ocamlopt.opt got signal and exited\", issue 'ulimit -s unlimited' and retry."
-                {failure & (arch = "ppc64" | arch = "arm64")}]
+maintainer: "Daniel Bünzli <daniel.buenzl i@erratique.ch>"
+authors: "The uucp programmers"
+license: "ISC"
+tags: ["unicode" "text" "character" "org:erratique"]
+homepage: "https://erratique.ch/software/uucp"
+doc: "https://erratique.ch/software/uucp/doc/"
+bug-reports: "https://github.com/dbuenzli/uucp/issues"
+depends: [
+  "ocaml" {>= "4.03.0"}
+  "ocamlfind" {build}
+  "ocamlbuild" {build}
+  "topkg" {build & >= "1.0.3"}
+  "uucd" {with-test}
+  "uunf" {with-test}
+  "uutf" {with-test}
+]
+depopts: ["uutf" "uunf" "cmdliner"]
+conflicts: [
+  "uutf" {< "1.0.1"}
+  "cmdliner" {< "1.1.0"}
+]
+build: [
+  "ocaml"
+  "pkg/pkg.ml"
+  "build"
+  "--dev-pkg"
+  "%{dev}%"
+  "--with-uutf"
+  "%{uutf:installed}%"
+  "--with-uunf"
+  "%{uunf:installed}%"
+  "--with-cmdliner"
+  "%{cmdliner:installed}%"
+]
+post-messages:
+  "If the build fails with \"ocamlopt.opt got signal and exited\", issue 'ulimit -s unlimited' and retry."
+    {failure & (arch = "ppc64" | arch = "arm64")}
+dev-repo: "git+https://erratique.ch/repos/uucp.git"

commit d3a2a43e19b3feb32e402cca513dbfe0d2b8f64f

Fix regression in ucharinfo introduced by 6d54a937df103ab.

B0.ml
@@ -22,7 +22,7 @@ let uucp_lib =
 let ucharinfo =
   let srcs = Fpath.[`File (v "test/ucharinfo.ml")] in
   let requires = [cmdliner; uutf; uunf; uucp] in
-  B0_ocaml.exe "uncharinfo" ~doc:"The ucharinfo tool" ~srcs ~requires
+  B0_ocaml.exe "ucharinfo" ~doc:"The ucharinfo tool" ~srcs ~requires
 
 (* Tests *)
 

doc/api.odocl
@@ -1,1 +1,0 @@
-Uucp

test/ucharinfo.ml
@@ -588,7 +588,7 @@ let ucharinfo =
 
 
 let main () = exit (Cmd.eval' ucharinfo)
-let main () = if !Sys.interactive then () else main ()
+let () = if !Sys.interactive then () else main ()
 
 (*---------------------------------------------------------------------------
    Copyright (c) 2017 The uucp programmers